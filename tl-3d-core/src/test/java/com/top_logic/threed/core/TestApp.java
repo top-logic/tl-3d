a158d293c1d13dc092406ddf56cc25f6e9b0629a
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-06-23 13:38:30 +0200

    Added new module for common 3D functionality.

---
+++ tl-3d-core/src/test/java/com/top_logic/threed/core/TestApp.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 My Company. All Rights Reserved
+ */
+package com.top_logic.threed.core;
+
+import junit.framework.TestCase;
+
+/**
+ * Test case for the app <code>TL Three.js Binding</code>. 
+ */
+public class TestApp extends TestCase {
+	
+	public void testIt() {
+		// Add some test.
+	}
+	
+}
