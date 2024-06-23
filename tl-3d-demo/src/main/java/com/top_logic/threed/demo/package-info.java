59da3a5e54689b8448e7c7337fabef87e70c1130
Bernhard Haumacher <bernhard.haumacher@top-logic.com>
2024-06-23 18:36:37 +0200

    Added demo application.

---
+++ tl-3d-demo/src/main/java/com/top_logic/threed/demo/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Custom extensions for 3D demo.
+ */
+package com.top_logic.threed.demo;
---
+++ tl-3d-demo/src/test/java/com/top_logic/threed/demo/TestApp.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 My Company. All Rights Reserved
+ */
+package com.top_logic.threed.demo;
+
+import junit.framework.TestCase;
+
+/**
+ * Test case for the app <code>TL 3D Demo</code>.
+ */
+public class TestApp extends TestCase {
+	
+	public void testIt() {
+		// Add some test.
+	}
+	
+}
