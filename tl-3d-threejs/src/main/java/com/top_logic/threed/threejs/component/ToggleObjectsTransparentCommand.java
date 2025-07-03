18e9689657d99cdef54616455308e9648d3fd15a
Daniel Busche <daniel.busche@top-logic.com>
2025-07-03 09:14:33 +0200

    Removed unnecessary display orders.

--- tl-3d-threejs/src/main/java/com/top_logic/threed/threejs/component/ToggleObjectsTransparentCommand.java
+++ tl-3d-threejs/src/main/java/com/top_logic/threed/threejs/component/ToggleObjectsTransparentCommand.java
@@ -8,7 +8,6 @@ package com.top_logic.threed.threejs.component;
 import com.top_logic.basic.config.InstantiationContext;
 import com.top_logic.basic.config.annotation.defaults.ClassDefault;
 import com.top_logic.basic.config.annotation.defaults.NullDefault;
-import com.top_logic.basic.config.order.DisplayOrder;
 import com.top_logic.layout.DisplayContext;
 import com.top_logic.layout.basic.ThemeImage;
 import com.top_logic.mig.html.layout.LayoutComponent;
@@ -16,21 +15,7 @@ import com.top_logic.tool.boundsec.CommandHandler;
 import com.top_logic.tool.boundsec.commandhandlers.ToggleCommandHandler;
 
 public class ToggleObjectsTransparentCommand extends ToggleCommandHandler {
-	@DisplayOrder({
-		Config.RESOURCE_KEY_PROPERTY_NAME,
-		Config.IMAGE_PROPERTY,
-		Config.DISABLED_IMAGE_PROPERTY,
-		Config.ACTIVE_IMAGE,
-		Config.ACTIVE_CSS_CLASSES,
-		Config.ACTIVE_RESOURCE_KEY,
-		Config.CLIQUE_PROPERTY,
-		Config.GROUP_PROPERTY,
-		Config.TARGET,
-		Config.EXECUTABILITY_PROPERTY,
-		Config.CONFIRM_PROPERTY,
-		Config.CONFIRM_MESSAGE,
-		Config.SECURITY_OBJECT,
-	})
+
 	public interface Config extends ToggleCommandHandler.Config {
 		@Override
 		@ClassDefault(ToggleObjectsTransparentCommand.class)
