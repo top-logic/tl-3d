8dc4106c9a5594ee3ae3652f71c607d774568997
Daniel Busche <daniel.busche@top-logic.com>
2025-07-18 12:52:16 +0200

    Added more SceneNode utility methods.

--- tl-3d-threejs/src/main/java/com/top_logic/threed/threejs/scene/SceneUtils.java
+++ tl-3d-threejs/src/main/java/com/top_logic/threed/threejs/scene/SceneUtils.java
@@ -5,9 +5,14 @@
  */
 package com.top_logic.threed.threejs.scene;
 
+import java.awt.Color;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
+import com.top_logic.basic.StringServices;
+import com.top_logic.layout.form.format.ColorFormat;
 import com.top_logic.threed.core.math.Transformation;
 import com.top_logic.threed.core.math.TransformationUtil;
 
@@ -17,6 +22,40 @@ import com.top_logic.threed.core.math.TransformationUtil;
  * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
  */
 public class SceneUtils {
+
+	/**
+	 * {@link SceneNode} visitor that applies a given consumer to each node in the subtree with the
+	 * visited node as root node.
+	 * 
+	 * @author <a href="mailto:daniel.busche@top-logic.com">Daniel Busche</a>
+	 */
+	public static class ForAllNodes
+			implements SceneNode.Visitor<Void, Consumer<? super SceneNode>, RuntimeException> {
+
+		/** Singleton {@link SceneUtils.ForAllNodes} instance. */
+		public static final SceneUtils.ForAllNodes INSTANCE = new SceneUtils.ForAllNodes();
+
+		/**
+		 * Creates a new {@link SceneUtils.ForAllNodes}.
+		 */
+		protected ForAllNodes() {
+			// singleton instance
+		}
+
+		@Override
+		public Void visit(GroupNode self, Consumer<? super SceneNode> arg) throws RuntimeException {
+			arg.accept(self);
+			self.getContents().forEach(content -> content.visit(this, arg));
+			return null;
+		}
+
+		@Override
+		public Void visit(PartNode self, Consumer<? super SceneNode> arg) throws RuntimeException {
+			arg.accept(self);
+			return null;
+		}
+
+	}
 
 	/**
 	 * Sets the given {@link Transformation} to the given node.
@@ -92,6 +131,30 @@ public class SceneUtils {
 			graph.setCoordinateSystem(Collections.emptyList());
 		} else {
 			graph.setCoordinateSystem(TransformationUtil.toList(tx));
+		}
+	}
+
+	/**
+	 * Adds all nodes from the graph to the given collection.
+	 */
+	public static <T extends Collection<? super SceneNode>> T collectAllNodes(SceneGraph graph, T out) {
+		SceneNode root = graph.getRoot();
+		if (root != null) {
+			root.visit(ForAllNodes.INSTANCE, out::add);
+		}
+		return out;
+	}
+
+	/**
+	 * Sets the given {@link Color} to the given node.
+	 *
+	 * @param color
+	 *        New color for the node. May be <code>null</code>.
+	 */
+	public static void setColor(SceneNode node, Color color) {
+		String colorString = color == null ? StringServices.EMPTY_STRING : ColorFormat.formatColor(color);
+		if (!colorString.equals(node.getColor())) {
+			node.setColor(colorString);
 		}
 	}
 }
